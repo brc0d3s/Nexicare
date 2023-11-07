a28f10df6315dadf0a3e6d2484c990e5498a9c5c
brc0d3s <brc0d3s@gmail.com>
2023-11-07 19:22:49 +0300

    nexicare

---
+++ app/src/main/java/com/example/nexicare/HealthArticlesDetailsActivity.java
@@ -0,0 +1,42 @@
+package com.example.nexicare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class HealthArticlesDetailsActivity extends AppCompatActivity {
+
+    TextView tv1;
+    ImageView img;
+    Button btnBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health_articles_details);
+
+        btnBack = findViewById(R.id.buttonHADBack);
+        tv1 = findViewById(R.id.textViewHADTitle);
+        img = findViewById(R.id.imageViewHAD);
+
+        Intent intent = getIntent();
+        tv1.setText(intent.getStringExtra("text1"));
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null){
+            int resId = bundle.getInt("text2");
+            img.setImageResource(resId);
+        }
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HealthArticlesDetailsActivity.this, HealthArticlesActivity.class));
+            }
+        });
+    }
+}
