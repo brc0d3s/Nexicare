194da39d64bf99fcaf31452a040b20f82d2f77c5
brc0d3s <brc0d3s@gmail.com>
2023-11-09 06:44:41 +0300

    nexicare

--- app/src/main/java/com/example/nexicare/BuyMedicineActivity.java
+++ app/src/main/java/com/example/nexicare/BuyMedicineActivity.java
@@ -2,13 +2,28 @@ package com.example.nexicare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BuyMedicineActivity extends AppCompatActivity {
+    private Button btnBack,btnGoToCart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_medicine);
+
+        btnBack=findViewById(R.id.buttonBMBack);
+        btnGoToCart=findViewById(R.id.buttonBMGoToCart);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(BuyMedicineActivity.this, HomeActivity.class));
+                finish();
+            }
+        });
     }
 }
