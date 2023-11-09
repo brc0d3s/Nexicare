ab621cd1948f446d703d0d206abee6a6245a0a4f
brc0d3s <brc0d3s@gmail.com>
2023-11-09 07:28:54 +0300

    isOnline

--- app/src/main/java/com/example/nexicare/LoginActivity.java
+++ app/src/main/java/com/example/nexicare/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.nexicare;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -39,33 +42,41 @@ public class LoginActivity extends AppCompatActivity {
                 if (username.isEmpty() || password.isEmpty()) {
                     Toast.makeText(getApplicationContext(), "Please fill in all details", Toast.LENGTH_SHORT).show();
                 } else {
-                    // Check the user's credentials in the Firebase database
-                    rootDatabaseRef.child(username).addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(DataSnapshot dataSnapshot) {
-                            if (dataSnapshot.exists()) {
-                                String storedPassword = dataSnapshot.child("password").getValue(String.class);
+                    if (isOnline()) { // Check for internet connectivity
+                        // Check the user's credentials in the Firebase database
+                        rootDatabaseRef.child(username).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                if (dataSnapshot.exists()) {
+                                    String storedPassword = dataSnapshot.child("password").getValue(String.class);
 
-                                if (password.equals(storedPassword)) {
-                                    // Pass the username to HomeActivity
-                                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                                    intent.putExtra("username", username);
-                                    startActivity(intent);
-                                    finish(); // Finish LoginActivity to prevent going back to it
+                                    if (password.equals(storedPassword)) {
+                                        // Pass the username to HomeActivity
+                                        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                        intent.putExtra("username", username);
+                                        startActivity(intent);
+                                        finish(); // Finish LoginActivity to prevent going back to it
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), "Incorrect password", Toast.LENGTH_SHORT).show();
+                                    }
                                 } else {
-                                    Toast.makeText(getApplicationContext(), "Incorrect password", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(getApplicationContext(), "User not found", Toast.LENGTH_SHORT).show();
                                 }
-                            } else {
-                                Toast.makeText(getApplicationContext(), "User not found", Toast.LENGTH_SHORT).show();
                             }
-                        }
 
-                        @Override
-                        public void onCancelled(DatabaseError databaseError) {
-                            // Handle any errors here
-                            Toast.makeText(getApplicationContext(), "Connect to Internet", Toast.LENGTH_SHORT).show();
-                        }
-                    });
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+                                if (databaseError.getCode() == DatabaseError.NETWORK_ERROR) {
+                                    Toast.makeText(getApplicationContext(), "Please connect to the internet", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    // Handle other database errors here
+                                    Toast.makeText(getApplicationContext(), "Database error: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Please connect to the internet", Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
         });
@@ -76,5 +87,11 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
             }
         });
+    }
+
+    private boolean isOnline() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
     }
 }
--- app/src/main/java/com/example/nexicare/RegisterActivity.java
+++ app/src/main/java/com/example/nexicare/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.example.nexicare;
 
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -55,6 +57,8 @@ public class RegisterActivity extends AppCompatActivity {
                     Toast.makeText(RegisterActivity.this, "Please fill in all details", Toast.LENGTH_SHORT).show();
                 } else if (!isValidEmail(email)) {
                     Toast.makeText(RegisterActivity.this, "Please enter a valid email address", Toast.LENGTH_SHORT).show();
+                } else if (!isOnline()) {
+                    Toast.makeText(RegisterActivity.this, "Please connect to the internet", Toast.LENGTH_SHORT).show();
                 } else {
                     if (password.equals(confirm)) {
                         if (isValidPassword(password)) {
@@ -79,7 +83,12 @@ public class RegisterActivity extends AppCompatActivity {
                 }
             }
         });
-        finish();
+    }
+
+    private boolean isOnline() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
     }
 
     public boolean isValidPassword(String password) {
